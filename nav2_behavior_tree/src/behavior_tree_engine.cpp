3989a03633d5a1247f2c466a872d30c571c2d5c5
ladianchad <qhrejddlvltm@gmail.com>
2023-12-07 12:43:56 +0900

    remove looprate log
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

--- nav2_behavior_tree/src/behavior_tree_engine.cpp
+++ nav2_behavior_tree/src/behavior_tree_engine.cpp
@@ -50,17 +50,14 @@ BehaviorTreeEngine::run(
         tree->rootNode()->halt();
         return BtStatus::CANCELED;
       }
-
       result = tree->tickRoot();
-
       onLoop();
-
-      if (!loopRate.sleep()) {
-        RCLCPP_WARN(
-          rclcpp::get_logger("BehaviorTreeEngine"),
-          "Behavior Tree tick rate %0.2f was exceeded!",
-          1.0 / (loopRate.period().count() * 1.0e-9));
-      }
+      // if (!loopRate.sleep()) {
+      //   RCLCPP_WARN(
+      //     rclcpp::get_logger("BehaviorTreeEngine"),
+      //     "Behavior Tree tick rate %0.2f was exceeded!",
+      //     1.0 / (loopRate.period().count() * 1.0e-9));
+      // }
     }
   } catch (const std::exception & ex) {
     RCLCPP_ERROR(

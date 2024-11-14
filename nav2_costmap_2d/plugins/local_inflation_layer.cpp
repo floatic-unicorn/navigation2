a1b4174dc57076d080fffa0ff014339e9bc44add
mkyun2 <robotics.gyun@gmail.com>
2024-11-14 17:09:10 +0900

    Reverted inscribed_radius

--- nav2_costmap_2d/plugins/local_inflation_layer.cpp
+++ nav2_costmap_2d/plugins/local_inflation_layer.cpp
@@ -87,6 +87,7 @@ LocalInflationLayer::onInitialize()
 {
   declareParameter("enabled", rclcpp::ParameterValue(true));
   declareParameter("inflation_radius", rclcpp::ParameterValue(0.55));
+  declareParameter("safety_margin", rclcpp::ParameterValue(0.0));
   declareParameter("cost_scaling_factor", rclcpp::ParameterValue(10.0));
   declareParameter("inflate_unknown", rclcpp::ParameterValue(false));
   declareParameter("inflate_around_unknown", rclcpp::ParameterValue(false));
@@ -98,6 +99,7 @@ LocalInflationLayer::onInitialize()
     }
     node->get_parameter(name_ + "." + "enabled", enabled_);
     node->get_parameter(name_ + "." + "inflation_radius", inflation_radius_);
+    node->get_parameter(name_ + "." + "safety_margin", inscribed_radius_);
     node->get_parameter(name_ + "." + "cost_scaling_factor", cost_scaling_factor_);
     node->get_parameter(name_ + "." + "inflate_unknown", inflate_unknown_);
     node->get_parameter(name_ + "." + "inflate_around_unknown", inflate_around_unknown_);
@@ -165,15 +167,15 @@ void
 LocalInflationLayer::onFootprintChanged()
 {
   std::lock_guard<Costmap2D::mutex_t> guard(*getMutex());
-  inscribed_radius_ = layered_costmap_->getInscribedRadius();
+  // inscribed_radius_ = layered_costmap_->getInscribedRadius();
   cell_inflation_radius_ = cellDistance(inflation_radius_);
   computeCaches();
   need_reinflation_ = true;
 
-  RCLCPP_DEBUG(
-    logger_, "InflationLayer::onFootprintChanged(): num footprint points: %zu,"
-    " inscribed_radius_ = %.3f, inflation_radius_ = %.3f",
-    layered_costmap_->getFootprint().size(), inscribed_radius_, inflation_radius_);
+  // RCLCPP_DEBUG(
+  //   logger_, "InflationLayer::onFootprintChanged(): num footprint points: %zu,"
+  //   " inscribed_radius_ = %.3f, inflation_radius_ = %.3f",
+  //   layered_costmap_->getFootprint().size(), inscribed_radius_, inflation_radius_);
 }
 
 void
